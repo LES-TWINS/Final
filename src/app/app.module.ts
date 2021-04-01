9a4e19ea615129109e8410e717d567a8752457f9
Temo <temogagna@gmail.com>
2021-04-01 21:13:07 +0400

    about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HeaderComponent } from './header/header.component';
+import { GamesComponent } from './games/games.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    HeaderComponent
+    HeaderComponent,
+    GamesComponent,
+    AboutComponent
     
   ],
   imports: [
